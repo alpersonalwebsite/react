760ad2e05c7d2e955b6eb313b9c6067fbb94fb9b
alpersonalwebsite <mypersonalwebsite@outlook.com>
2018-09-05 13:45:26 -0700

    Commenting bindActionCreators

--- basic-redux-example[redux-thunk]/App.js
+++ basic-redux-example[redux-thunk]/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+//import { bindActionCreators } from 'redux';
 import { fetchComments } from './actions';
 
 class App extends Component {
