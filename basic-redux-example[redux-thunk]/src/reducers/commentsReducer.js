f335326b7bba25eb95a1f4e91c4e8bab8802831f
alpersonalwebsite <mypersonalwebsite@outlook.com>
2018-11-09 14:31:30 -0800

    improve reducer and add lodash

--- basic-redux-example[redux-thunk]/src/reducers/commentsReducer.js
+++ basic-redux-example[redux-thunk]/src/reducers/commentsReducer.js
@@ -3,8 +3,11 @@ import { FETCH_COMMENTS } from '../actions/types';
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_COMMENTS:
-      //return action.payload.data;
-      return action.payload;
+      const commentsPayload =
+        action.payload.length > 10
+          ? action.payload.slice(0, 10)
+          : action.payload;
+      return [...state, ...commentsPayload];
 
     default:
       return state;
